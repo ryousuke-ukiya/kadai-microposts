08f1fe830f1816e61ea3ab7db6c89641d6470233
ryousuke.ukiya <hoppenigo@yahoo.co.jp>
2020-05-03 10:38:06 +0000

    kadai3

--- resources/views/user_favorite/favorite_button.blade.php
+++ resources/views/user_favorite/favorite_button.blade.php
@@ -1,9 +1,11 @@
 
     @if (Auth::user()->is_favorites($micropost->id))
+    @csrf
         {!! Form::open(['route' => ['favorites.unfavorite', $micropost->id], 'method' => 'delete']) !!}
             {!! Form::submit('Unfavorite', ['class' => "btn btn-success btn-block btn-sm "]) !!}
         {!! Form::close() !!}
     @else
+    @csrf
         {!! Form::open(['route' => ['favorites.favorite', $micropost->id]]) !!}
             {!! Form::submit('Favorite', ['class' => "btn btn-light btn-block btn-sm "]) !!}
         {!! Form::close() !!}
--- resources/views/user_follow/follow_button.blade.php
+++ resources/views/user_follow/follow_button.blade.php
@@ -1,9 +1,11 @@
 @if (Auth::id() != $user->id)
     @if (Auth::user()->is_following($user->id))
+    @csrf
         {!! Form::open(['route' => ['user.unfollow', $user->id], 'method' => 'delete']) !!}
             {!! Form::submit('Unfollow', ['class' => "btn btn-danger btn-block"]) !!}
         {!! Form::close() !!}
     @else
+    @csrf
         {!! Form::open(['route' => ['user.follow', $user->id]]) !!}
             {!! Form::submit('Follow', ['class' => "btn btn-primary btn-block"]) !!}
         {!! Form::close() !!}
